d8369740f10aed77c7ad70d11afe1c640b402014
Nitin Prakash <prakash.nitin63@gmail.com>
2019-03-29 20:16:33 +0530

    model embeddings

---
+++ embeddings.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+
+class ModelEmbeddings(nn.Module):
+    def __init__(self, embed_size, vocab):
+        """
+        Init the Embedding layers.
+
+        @param embed_size (int): Embedding size (dimensionality)
+        @param vocab (Vocab): Vocabulary object containing src and tgt languages
+                              See vocab.py for documentation.
+        """
+        super(ModelEmbeddings, self).__init__()
+        self.embed_size = embed_size
+
+        self.source = None
+        self.target = None
+
+        src_pad_token_idx = vocab.src['<pad>']
+        tgt_pad_token_idx = vocab.tgt['<pad>']
+
+        self.source = nn.Embedding(
+            len(vocab.src), embed_size, padding_idx=src_pad_token_idx)
+        self.target = nn.Embedding(
+            len(vocab.tgt), embed_size, padding_idx=tgt_pad_token_idx)
