e8436026a8acd84f9811a56a12f1142e9d6274f8
Nitin Prakash <prakash.nitin63@gmail.com>
2019-03-30 19:19:53 +0530

    model

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# read_corpus() taken from https://github.com/pcyin/pytorch_nmt
+# read_corpus() and batch_iter() taken from https://github.com/pcyin/pytorch_nmt
 
 
 def pad_sents(sents, pad_token):
@@ -31,6 +31,8 @@ def pad_sents(sents, pad_token):
             sents[k][i] if len(sents[k]) > i else pad_token
             for k in range(batch_size)
         ])
+    
+    sents_padded = list(map(list, zip(*sents_padded)))
 
     return sents_padded
 
@@ -50,3 +52,25 @@ def read_corpus(file_path, source):
 
     return data
 
+
+def batch_iter(data, batch_size, shuffle=False):
+    """ Yield batches of source and target sentences reverse sorted by length (largest to smallest).
+    @param data (list of (src_sent, tgt_sent)): list of tuples containing source and target sentence
+    @param batch_size (int): batch size
+    @param shuffle (boolean): whether to randomly shuffle the dataset
+    """
+    batch_num = math.ceil(len(data) / batch_size)
+    index_array = list(range(len(data)))
+
+    if shuffle:
+        np.random.shuffle(index_array)
+
+    for i in range(batch_num):
+        indices = index_array[i * batch_size:(i + 1) * batch_size]
+        examples = [data[idx] for idx in indices]
+
+        examples = sorted(examples, key=lambda e: len(e[0]), reverse=True)
+        src_sents = [e[0] for e in examples]
+        tgt_sents = [e[1] for e in examples]
+
+        yield src_sents, tgt_sents
